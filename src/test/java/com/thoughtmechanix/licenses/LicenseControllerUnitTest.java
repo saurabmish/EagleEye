a6ed0d072f7b4fa3cc11d4ef9e48aafd803dbd44
Saurabh Mishra <saurab.mish@gmail.com>
2021-01-24 18:11:45 -0800

    test return value of controller

---
+++ src/test/java/com/thoughtmechanix/licenses/LicenseControllerUnitTest.java
@@ -0,0 +1,29 @@
+package com.thoughtmechanix.licenses;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@WebMvcTest(LicenseController.class)
+@ExtendWith(MockitoExtension.class)
+public class LicenseControllerUnitTest {
+    
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().string(equalTo("Hello, World!")));
+    }
+}
