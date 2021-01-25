d94b750710daa9e9fed2cea574c5007b0eb252d3
Saurabh Mishra <saurab.mish@gmail.com>
2021-01-25 11:45:06 -0800

    incorporated license service mock bean

--- src/test/java/com/thoughtmechanix/licenses/LicenseControllerUnitTest.java
+++ src/test/java/com/thoughtmechanix/licenses/LicenseControllerUnitTest.java
@@ -1,17 +1,19 @@
 package com.thoughtmechanix.licenses;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.BDDMockito.given;
 
 @WebMvcTest(LicenseController.class)
 @ExtendWith(MockitoExtension.class)
@@ -20,8 +22,13 @@ public class LicenseControllerUnitTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private LicenseService licenseService;
+
     @Test
     public void getHello() throws Exception {
+        given(licenseService.greet()).willReturn("Hello, World!");
+
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(content().string(equalTo("Hello, World!")));
