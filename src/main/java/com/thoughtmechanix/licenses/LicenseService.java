fa3b11f1586cc0f61d3b8c1c4db074150dd173f5
Saurabh Mishra <saurab.mish@gmail.com>
2021-01-25 11:42:12 -0800

    return String 'Hello, World!'

---
+++ src/main/java/com/thoughtmechanix/licenses/LicenseService.java
@@ -0,0 +1,11 @@
+package com.thoughtmechanix.licenses;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LicenseService {
+ 
+    public String greet() {
+        return "Hello, World!";
+    }
+}
